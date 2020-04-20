dccfa9b6912328662a6fac66551afc584c129995
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-04-20 10:09:35 +0200

    first commit

---
+++ src/main/java/com/teameast/parkshark/ParksharkApplication.java
@@ -0,0 +1,13 @@
+package com.teameast.parkshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksharkApplication.class, args);
+	}
+
+}
