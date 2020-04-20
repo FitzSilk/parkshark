e4a979fd6dd027981a13c8e6fe2a0cc65d546e0f
TWENS-VDAB <thomas.wens@vdab.be>
2020-04-20 11:50:38 +0200

    added logging and it works

--- api/src/main/java/com/teameast/parkshark/api/ParkingLotController.java
+++ api/src/main/java/com/teameast/parkshark/api/ParkingLotController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/ParkingLot")
+@RequestMapping(path = "/parkingLot")
 
 public class ParkingLotController {
     private final Logger myLogger = LoggerFactory.getLogger(ParkingLotController.class);
